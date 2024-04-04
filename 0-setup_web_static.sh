39d8e83031881cba7187bc4621cbaad5be97a205
mg5714 <mistergddora2000@gmail.com>
2024-04-04 03:17:55 +0200

    go

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,37 +1,27 @@
 #!/usr/bin/env bash
-# Install Nginx if not already installed
-if ! dpkg -l | grep -q nginx; then
-    apt-get update
-    apt-get -y install nginx
-fi
+# Bash script that setup web servers for deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-# Create necessary directories if they don't exist
-mkdir -p /data/web_static/{releases/test,shared}
-
-# Create a fake HTML file for testing Nginx configuration
-echo "<html>
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create or recreate symbolic link
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-# Set ownership of /data/ folder to ubuntu user and group recursively
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration to serve content of /data/web_static/current/ to hbnb_static
-NGINX_CONFIG="/etc/nginx/sites-available/default"
-NGINX_ALIAS="location /hbnb_static {\n    alias /data/web_static/current/;\n}"
-if ! grep -q "location /hbnb_static" "$NGINX_CONFIG"; then
-    sed -i "/server_name _;/a $NGINX_ALIAS" "$NGINX_CONFIG"
-fi
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-# Restart Nginx
-service nginx restart
-
-# Exit successfully
-exit 0
+sudo service nginx restart
