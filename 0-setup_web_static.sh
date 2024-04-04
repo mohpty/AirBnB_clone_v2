b77e5c54ceb1aab1da05fa543836ed405c16161a
mg5714 <mistergddora2000@gmail.com>
2024-04-04 03:01:19 +0200

    go

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,29 +1,12 @@
 #!/usr/bin/env bash
-# Install Nginx if not already installed
-if ! command -v nginx &> /dev/null; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-fi
+# sets up the web servers for the deployment of web_static
 
-# Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/{releases/test,shared}
-sudo touch /data/web_static/releases/test/index.html
-
-# Create symbolic link to the test folder
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Set ownership of /data/ folder to ubuntu user and group recursively
-sudo chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration to serve content of /data/web_static/current/ to hbnb_static
-CONFIG_PATH="/etc/nginx/sites-available/default"
-NGINX_ALIAS="location /hbnb_static {\n    alias /data/web_static/current/;\n}"
-if ! sudo grep -q "location /hbnb_static" "$CONFIG_PATH"; then
-    sudo sed -i "/server_name _;/a $NGINX_ALIAS" "$CONFIG_PATH"
-fi
-
-# Restart Nginx
-sudo service nginx restart
-
-# Exit successfully
-exit 0
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
