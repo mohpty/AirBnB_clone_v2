2b61fe4bf0684864fad7805793b8ef094c4952af
mg5714 <mistergddora2000@gmail.com>
2024-04-20 16:41:54 +0200

    bye

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application"""
+"""Script that starts Flask web application"""
 
 from flask import Flask, render_template
 from models import storage
